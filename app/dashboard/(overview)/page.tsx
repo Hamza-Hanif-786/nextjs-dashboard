5fe7cac15b2c8c67a85718a2fa965d9a52775bb7
Hamza Hanif <hamzahanif1024@gmail.com>
2023-12-25 11:18:09 +0500

    font lusitana update

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,7 +10,7 @@ import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardSkeleton  } from '@/a
 export default async function Page(){
     return (
         <main>
-            <h1 className={'${lusitana.className} mb-4 text-xl md:text-2xl'}>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
